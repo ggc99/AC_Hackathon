9afab3cc231c6bbf3cfda063180928b0429fba1a
ggc99 <lgustavogarcial@gmail.com>
2022-04-02 18:34:19 -0500

    fixed routing

---
+++ ui/src/UserScan.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Login from './Login';
+import Scan from './Scan';
+import Finished from './Finished';
+
+class UserScan extends React.Component {
+  state = { teamId: "", video: null, currentPage: "login" };
+
+  onIdSubmit = (id) => {
+    this.setState({ teamId: id });
+    this.setState({ currentPage: "scan" });
+  };
+
+  onUploadClick = (uploadedVideo) => {
+    this.setState({ video: uploadedVideo });
+    this.setState({ currentPage: "finished" });
+  }
+
+  render() {
+    if (this.state.currentPage === "login") {
+      return (
+        <div>
+          <Login onIdSubmit={this.onIdSubmit} />
+        </div>
+      )
+    } else if (this.state.currentPage === "scan" ) {
+      return <Scan teamId={this.state.teamId} onUploadClick={this.onUploadClick}/>
+    } else if (this.state.currentPage === "finished") {
+      return <Finished teamId={this.state.teamId} video={this.state.video} /> 
+    }
+  }
+}
+
+export default UserScan;
