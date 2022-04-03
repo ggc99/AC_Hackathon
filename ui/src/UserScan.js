345047164ed00a85649e1264510a77b3512d964f
ggc99 <lgustavogarcial@gmail.com>
2022-04-03 01:06:31 -0500

    thermal screening changes

--- ui/src/UserScan.js
+++ ui/src/UserScan.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Login from './Login';
 import Scan from './Scan';
 import Finished from './Finished';
+import WebcamScan from './WebcamScan';
 
 class UserScan extends React.Component {
   state = { teamId: "", video: null, currentPage: "login" };
@@ -21,11 +22,13 @@ class UserScan extends React.Component {
     if (this.state.currentPage === "login") {
       return (
         <div>
+
           <Login onIdSubmit={this.onIdSubmit} />
         </div>
       )
     } else if (this.state.currentPage === "scan" ) {
       return <Scan teamId={this.state.teamId} onUploadClick={this.onUploadClick}/>
+        // return <WebcamScan></WebcamScan>
     } else if (this.state.currentPage === "finished") {
       return <Finished teamId={this.state.teamId} video={this.state.video} /> 
     }
--- ui/src/WebcamScreenCapture.js
+++ ui/src/WebcamScreenCapture.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Webcam from "react-webcam";
+import './WebcamScreenCapture.css';
+
 
 const WebcamStreamCapture = ({onUploadClick}) => {
   const webcamRef = React.useRef(null);
   const mediaRecorderRef = React.useRef(null);
   const [capturing, setCapturing] = React.useState(false);
+  const [captureIdle, setCaptureIdle] = React.useState(true);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
     
     const handleStartCaptureClick = React.useCallback(() => {
@@ -39,14 +42,31 @@ const WebcamStreamCapture = ({onUploadClick}) => {
       onUploadClick(url);
   }, [mediaRecorderRef, webcamRef, setCapturing]);
 
+  const handleRecordStart = React.useCallback(() => {
+    setCaptureIdle(false);
+    setTimeout(() => {
+      onUploadClick("https://ac-hackathon.s3.us-east-2.amazonaws.com/thermal_video_10s_crop.mp4")
+    }, 4000);
+  });
+
   return (
     <>
-      <Webcam audio={false} ref={webcamRef} />
-      {capturing ? (
-        <p>Place your face in the frame</p>
+      {/* <Webcam audio={false} ref={webcamRef} /> */}
+        {/* <p>Place your face in the frame</p>
       ) : (
         <button onClick={handleStartCaptureClick}>Start Capture</button>
-      )}
+      )} */}
+      <div className="wrapper">
+          { captureIdle ? <div className="flash-text">Please align your face</div> : null }
+          { !captureIdle ? <div style={{ color: "red" }} class="flash-text">Screening</div> : null }
+          <div className="video-content">
+          { captureIdle ? <img id="video-overlay" src="https://ac-hackathon.s3.us-east-2.amazonaws.com/face-outline.png"></img> : null }
+            {/* <video src="https://ac-hackathon.s3.us-east-2.amazonaws.com/thermal_video_10s_crop.mp4" autoPlay ></video> */}
+            <video src="https://ac-hackathon.s3.us-east-2.amazonaws.com/toma_thermal.mp4" autoPlay ></video>
+          </div>
+        <button onClick={handleRecordStart}>Start</button>
+      </div>
+      
     </>
   );
 };
